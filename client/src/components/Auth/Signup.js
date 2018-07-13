b676cb6e453d812db252c714a2f24bdd2b13b9dd
Jeremy Black <jb@yodacom.com>
2018-07-13 10:21:51 -0600

    added form with state

---
+++ client/src/components/Auth/Signup.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Signup extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: ""
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { username, email, password, passwordConfirmation } = this.state;
+    return (
+      <div className="App">
+        <h2>Signup</h2>
+        <form className="form">
+          <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange} />
+          <input type="email" name="email" placeholder="Email" value={email} onChange={this.handleChange} />
+          <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange} />
+          <input type="password" name="passwordConfirmation" placeholder="Confirm Password" value={passwordConfirmation} onChange={this.handleChange} />
+          <button type="submit" className="button-primary">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Signup;
